f6736e0b1b102056cb6ac212f67e0a1c7ceb716d
Rais Kazi <56422384+raiskazi@users.noreply.github.com>
2022-11-23 20:26:47 -0600

    Image uplaod works

---
+++ frontend/src/App.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+import React,{Component} from 'react';
+
+class App extends Component {
+
+	state = {
+
+	// Initially, no file is selected
+	selectedFile: null
+	};
+	
+	// On file select (from the pop up)
+	onFileChange = event => {
+	
+	// Update the state
+	this.setState({ selectedFile: event.target.files[0] });
+	
+	};
+	
+	// On file upload (click the upload button)
+	onFileUpload = () => {
+	
+	// Create an object of formData
+	const formData = new FormData();
+	
+	// Update the formData object
+	formData.append(
+		"myFile",
+		this.state.selectedFile,
+		this.state.selectedFile.name
+	);
+	
+	// Details of the uploaded file
+	console.log(this.state.selectedFile);
+	
+	// Request made to the backend api
+	// Send formData object
+	axios.post("api/uploadfile", formData);
+	};
+	
+	// File content to be displayed after
+	// file upload is complete
+	fileData = () => {
+	
+	if (this.state.selectedFile) {
+		
+		return (
+		<div>
+			<h2>File Details:</h2>
+			<img src={this.state.selectedFile}/>
+			<p>File Name: {this.state.selectedFile.name}</p>
+			<p>File Type: {this.state.selectedFile.type}</p>
+			<p>
+			Last Modified:{" "}
+			{this.state.selectedFile.lastModifiedDate.toDateString()}
+			</p>
+
+		</div>
+		);
+	} else {
+		return (
+		<div>
+			<br />
+		</div>
+		);
+	}
+	};
+	
+	render() {
+	
+	return (
+		<div>
+			<h2>
+			Upload your AI-Generated Image!
+			</h2>
+
+			<h4>
+				Visit <a href="https://huggingface.co/spaces/stabilityai/stable-diffusion">Stable Diffusion</a> to create an AI-generated image
+			</h4>
+
+			<br></br>
+
+			<div>
+				<input type="file" onChange={this.onFileChange} />
+				<button onClick={this.onFileUpload}>
+				Submit
+				</button>
+			</div>
+		{this.fileData()}
+		</div>
+	);
+	}
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import render from 'react-dom';
+import ReactDOM from 'react-dom'
+import {useState} from "react"
+// import { Button, ChakraProvider } from "@chakra-ui/react";
+
+import './index.css';
+// import Header from "./components/Header";
+import App from './App';
+
+// function App() {
+//   // return (
+//   //   <ChakraProvider>
+//   //     <Header />
+//   //   </ChakraProvider>
+//   // );
+
+//   const [file, setFile] = useState();
+//     function handleChange(e) {
+//         console.log(e.target.files);
+//         setFile(URL.createObjectURL(e.target.files[0]));
+//     }
+  
+//     return (
+//         <div className="App">
+//             <ChakraProvider>
+//               <Header />
+//             </ChakraProvider>
+
+//             <h2>--Upload your AI-Generated Image:</h2>
+//             <input type="file" onChange={handleChange} />
+//             <img src={file}/>
+//             <Button> Submit</Button>
+//         </div>
+//     );
+// }
+
+// const rootElement = document.getElementById("root")
+// render(<App />, rootElement)
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+  );
+  
+  const app = (
+    <React.StrictMode>
+      <App />  
+    </React.StrictMode>
+  )
+  
+  root.render(app);
