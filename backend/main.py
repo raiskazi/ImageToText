03dfb753df1bf27268cd09b16bc5ea548a6cea49
Rais Kazi <56422384+raiskazi@users.noreply.github.com>
2022-11-23 21:18:49 -0600

    backend handling file structure

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,16 @@
 import uvicorn
+from typing import Optional, List
+from fastapi import FastAPI, HTTPException, UploadFile
+import aiofiles
 
+app = FastAPI()
 
 if __name__ == "__main__":
     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+
+@app.post("/api/uploadfile")
+async def create_upload_file(file: UploadFile = File(...)):
+    out_path = 'example/path/file'
+    async with aiofiles.open(out_path, 'wb') as out_file:
+        content = await file.read()
+        await out_file.write(content)
