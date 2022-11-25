f3085a4e9be5094106da85181412891e58d15fba
Rais Kazi <56422384+raiskazi@users.noreply.github.com>
2022-11-25 15:06:37 -0600

    Next.js file upload working

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,42 @@
+# fastapi boilerplate
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import File, UploadFile
+import uvicorn
+import io
+import os
+import requests
+from PIL import Image
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def prediction():
+    return 0
+
+@app.post("/api/upload")
+async def upload(file = File(...)):
+    print(file.filename)
+    return {"filename": file.filename}
+
+
+
+
+
+
+# REACT CODE BELOW
+
 # import uvicorn
 # from typing import Optional, List
 # from fastapi import FastAPI, HTTPException, UploadFile, File
@@ -22,9 +61,6 @@
 #     allow_headers=["*"],
 # )
 
-# def prediction():
-#     return 0
-
 # @app.post("/api/uploadfile")
 # async def create_upload_file(file: UploadFile = File(...)):
 #     # out_path = 'example/path/file'
@@ -39,7 +75,6 @@
 #     print(file.filename)
 
 #     return {"filename": file.filename}
-
 # # if __name__ == "__main__":
 # #     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
 
@@ -50,26 +85,4 @@
 
 
 
-# fastapi boilerplate
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import File, UploadFile
 
-app = FastAPI()
-
-origins = [
-    "http://localhost:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.post("/api/upload")
-async def upload(file = File(...)):
-    print(file.filename)
-    return {"filename": file.filename}
